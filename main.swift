676b36968d9ae404db7babfb62df8922364e4c2c
Zoltán Majoros <zoltan@arcanelab.com>
2017-04-14 02:35:57 +0200

    Code cleanup

--- main.swift
+++ main.swift
@@ -30,18 +30,16 @@ extension RGBColor: Equatable, Hashable
 
 if CommandLine.argc != 2
 {
-    print("usage: \(CommandLine.arguments[0]) ImageFileName")
+    print("Usage: \(CommandLine.arguments[0]) ImageFileName")
     exit(0)
 }
 
 let filePath = CommandLine.arguments[1]
-let fileManager = FileManager.default
-
 let url = URL(fileURLWithPath: filePath)
 
 guard let imageSource = CGImageSourceCreateWithURL(url as CFURL!, nil) else
 {
-    print("Error while loading image")
+    print("File not found")
     exit(1)
 }
 
@@ -51,14 +49,13 @@ guard let image = CGImageSourceCreateImageAtIndex(imageSource, 0, nil) else
     exit(1)
 }
 
-print("Image dimensions: \(image.width)x\(image.height)\n")
+print("Image dimensions: \(image.width)x\(image.height)")
 //print("\(image.bitsPerPixel) bits per pixel")
 
+print("Calculating...")
 let pixelPointer: UnsafePointer<uint8> = CFDataGetBytePtr(image.dataProvider?.data)
-
-var colors = Array<RGBColor>()
-
-guard image.width > 0 && image.height > 0 else { exit(0) }
+var palette = Array<RGBColor>()
+guard image.width > 0 && image.height > 0 else { exit(1) }
 
 for y in 0..<image.height
 {
@@ -70,16 +67,17 @@ for y in 0..<image.height
         let g = pixelPointer[offsetX+1]
         let b = pixelPointer[offsetX+2]
         let currentColor = RGBColor(red: UInt16(r), green: UInt16(g), blue: UInt16(b))
-        if colors.contains(where: { $0 == currentColor }) { continue }
-        colors.append(currentColor)
+        if palette.contains(where: { $0 == currentColor }) { continue }
+        palette.append(currentColor)
     }
 }
 
 //print("The image has \(colors.count) different colors.")
 var i = 0;
-for color in colors
+for color in palette
 {
     let hexColor = String(format: "#%06X", color.hashValue)
     print("palette[\(i)] = \(hexColor) (\(color.red),\(color.green),\(color.blue))");
     i = i+1
 }
+
